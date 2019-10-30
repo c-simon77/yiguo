43034f65f43e44517bdefcd7473aecd353d9cd93
e <17860576727@163.com>
2019-10-30 17:35:27 +0800

    yg

---
+++ yiguo/src/views/Eat/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="eat">
+
+    <EatCont></EatCont> 
+    <TabBar></TabBar>
+  </div>
+
+</template>
+
+<script>
+import EatCont from './EatCont'
+import TabBar from "@/components/TabBar"
+export default {
+    components:{
+      EatCont,TabBar
+      
+    }
+}
+</script>
+
+<style>
+
+</style>
